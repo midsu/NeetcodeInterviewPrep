8c6eeadbee6e68fa82d530b44b0dfc414ef5a503
Hamid Suha <34997278+midsu@users.noreply.github.com>
2023-04-13 22:15:24 -0700

    Add files via upload

---
+++ validSudoku.py
@@ -0,0 +1,11 @@
+# Determine if a 9 x9 Sudoku is valid. Only the filled cell 
+# need to be validated "according to the following rules": 
+# 1. Each row must contain the digits 1 - 9 without repitition. 
+# 2. Each column must contain digits 1 - 9 without repitition. 
+# 3. Each of the nine 3 x 3 sub-boxed of grid must contain the digits 1- 9 without repitition
+
+# NOTES:
+# - A sudoku board (partially filled) could be valid but is not necessarily solvable.
+# - Only the filled cells need to be validated according to the mentioned rules.
+
+
