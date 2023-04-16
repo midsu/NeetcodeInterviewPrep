0e376150a6e86e2c7e511e39998d075bae1a2457
Hamid Suha <34997278+midsu@users.noreply.github.com>
2023-04-15 22:35:26 -0700

    added comments

--- longestConsecutive.py
+++ longestConsecutive.py
@@ -7,16 +7,29 @@
 # Explanation: The longest consecutive elements sequence is [1, 2, 3, 4], 
 # therefore its length is 4
 
+
 class Solution:
-    def longestConsecutive(sekf, nums: List[int]) -> int:
-        numset = set(nums)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # Convert the list of integers into a set for fast lookup
+        numSet = set(nums)
+        
+        # Initialize the longest sequence length to zero
         longest = 0
 
+        # Loop through each number in the input list
         for n in nums:
-            # check if its the start of a sequence
-            if (n-1) not in numSet:
+            # Check if the number is the start of a sequence by checking if the previous number is not in the set
+            if (n - 1) not in numSet:
+                # If it is the start of a sequence, find the length of the sequence by incrementing the number until it is no longer in the set
                 length = 0
                 while (n + length) in numSet:
-                    length+=1
+                    length += 1
+                
+                # Update the longest sequence length if the current sequence is longer
                 longest = max(length, longest)
+        
+        # Return the length of the longest sequence found
         return longest
+
+    # O(n) time complexity
+    # O(n) space complexity 
