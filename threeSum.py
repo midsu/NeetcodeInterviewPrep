3bc42555ff9d73181e9ebc0cc3b22bbcdc560df4
Hamid Suha <34997278+midsu@users.noreply.github.com>
2023-04-21 11:46:38 -0700

    finished (without comment)
    will add comment next

--- threeSum.py
+++ threeSum.py
@@ -24,4 +24,17 @@ class Solution:
 
             l, r = i + 1, len(nums) - 1
             while l < r:
-                threeSum = a +
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
+
+
+                    
