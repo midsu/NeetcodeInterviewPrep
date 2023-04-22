e1d58a11ee5e871b5486137a2a18b4b40b2fd91e
Hamid Suha <34997278+midsu@users.noreply.github.com>
2023-04-21 22:48:42 -0700

    Update twoSum2.py

--- twoSum2.py
+++ twoSum2.py
@@ -10,6 +10,24 @@
 
 # EXAMPLE:
 
+
 # Input: numbers = [2, 7, 11, 15], target = 9
 # Output: [1,2]
-# Exmplanation: The sum of 2 and 7 is 9, therefore index1 = 1, index2 = 2
+# Exmplanation: (index here started from 1, not 0). The sum of 2 and 7 is 9,
+# therefore index1 = 1, index2 = 2 
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
