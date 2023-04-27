1a1d7cbff8ea5d7cc78c2ac075200985728efae9
Hamid Suha <34997278+midsu@users.noreply.github.com>
2023-04-26 22:40:22 -0700

    added comment

--- topkfrequentElements.py
+++ topkfrequentElements.py
@@ -8,20 +8,27 @@
 # Solution using "bucket sort"
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-    count = {}
-    freq = [[] for i in range(len(nums)+1)]
-
-    for n in nums:
-        count[n] = 1 + count.get(n, 0)
-    for n, c in count.items():
-        freq[c].append(n)
-
-    res = []
-    for i in range(len(freq) - 1, 0, -1):
-        for n in freq[i]:
-            res.append(n)
-            if len(res) == k:
-                return res
-
+        # Initialize an empty dictionary to count the frequency of each integer
+        count = {}
+        
+        # Initialize a list of empty lists to group integers by frequency
+        freq = [[] for i in range(len(nums)+1)]
+        
+        # Iterate through the input list and update the frequency count for each integer
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        
+        # Iterate through the count dictionary and group integers by frequency in the freq list
+        for n, c in count.items():
+            freq[c].append(n)
+        
+        # Initialize an empty list to hold the top k frequent integers
+        res = []
+        
+        # Iterate through the freq list backwards and add integers to res until its length is k
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
 # O(n) time complexity
-
