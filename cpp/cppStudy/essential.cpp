2292dc508d397f0598498967dd1366c73bac1f8c
Hamid Suha <34997278+midsu@users.noreply.github.com>
2023-07-03 13:29:57 -0700

    Add files via upload

---
+++ cpp/cppStudy/essential.cpp
@@ -0,0 +1,14 @@
+#include <format>
+#include <iostream>
+
+using std::format;
+using std::cout;
+
+int main(){
+    const char string[] {"string"};
+
+    for(int i {0}; string[i]; ++i){
+        count<<format("element {} is {} \n, i, string[i]");
+    }
+
+}
---
+++ cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+
+}
