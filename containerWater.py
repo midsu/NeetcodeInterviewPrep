0259daba15e0a7f303d4846b6037ea7174c67caf
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-10 19:51:32 -0700

    cont

---
+++ containerWater.py
@@ -0,0 +1,12 @@
+'''
+Given n non-negative intergers where eah represent a point of coordinate (i, a1) . 
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+find two lines, which together with x-axis, forms a container constains the most water.
+
+Input: height [1, 8, 6, 2, 5, 4, 8, 3, 7] 
+Output: 49
+Explaination; the vertical lines are represented by array [1, 8, 6, 2, 5, 4, 8, 3, 7], 
+In this case the max area of water(blue section), the container can contain is 49.
+
+'''
+
--- threeSum.py
+++ threeSum.py
@@ -12,29 +12,30 @@
 #   [-1, 0, 1],
 #   [-1, -1, 2]   
 # ]
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
+        res = []  # Initialize a list to store the resulting triplets
+        nums.sort()  # Sort the input array in ascending order
 
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        for i, a in enumerate(nums):  # Iterate over the elements of the sorted array
+            if i > 0 and a == nums[i - 1]:  # Skip duplicate elements to avoid duplicate triplets
                 continue
 
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
+            l, r = i + 1, len(nums) - 1  # Initialize two pointers: l points to the element after a, r points to the last element
+
+            while l < r:  # Loop until the two pointers meet
+                threeSum = a + nums[l] + nums[r]  # Calculate the sum of the three elements
+
+                if threeSum > 0:  # If the sum is greater than 0, decrement r to get a smaller element
                     r -= 1
-                elif threeSum < 0:
+                elif threeSum < 0:  # If the sum is less than 0, increment l to get a larger element
                     l += 1
-                else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
+                else:  # If the sum is 0, a valid triplet is found
+                    res.append([a, nums[l], nums[r]])  # Add the triplet to the result list
+                    l += 1  # Increment l to move to the next unique element
+
+                    while nums[l] == nums[l - 1] and l < r:  # Skip duplicate elements to avoid duplicate triplets
                         l += 1
-        return res
+        return res  # Return the list of unique triplets that sum up to zero
 
-
-                    
+# example
