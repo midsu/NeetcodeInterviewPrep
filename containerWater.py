1ed4eb6afe09f652257f3c63cfcd629610b45a48
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-12 13:24:07 -0700

    max container area problem

--- containerWater.py
+++ containerWater.py
@@ -10,3 +10,21 @@ In this case the max area of water(blue section), the container can contain is 4
 
 '''
 
+class solution:
+    def maxArea(self, height: list[int]) -> int:
+        res = 0
+        l, r = 0, len(height) - 1
+
+        while l < r:
+            # Calculate the area between the lines at indices l and r
+            area = (r - l) * min(height[l], height[r])
+            # Update the maximum area if the calculated area is greater
+            res = max(area, res)
+
+            # Move the index l or r towards the center based on which line is shorter
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        
+        return res
