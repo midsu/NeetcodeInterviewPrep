4a1237678aaf6ed8d2776c72d6457415baac9680
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-14 23:09:23 -0700

    comment

--- maxProfit.py
+++ maxProfit.py
@@ -1,4 +1,13 @@
+'''
+Given an array which its elements are the prices,
+With one transaction, find the max profit of a trade
 
+Example:
+input: [7, 1, 5, 3, 6, 4]
+output: 5
+
+Explanation: Buy on day 2 (1), sell on day 5 (6), with profit = 6 - 1 = 5
+'''
 
 
 class Solution:
