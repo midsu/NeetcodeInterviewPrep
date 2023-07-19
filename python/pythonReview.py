d973de16a1b706bc30d769c075fdac89f311e853
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-18 17:08:31 -0700

    SET

--- python/pythonReview.py
+++ python/pythonReview.py
@@ -145,3 +145,26 @@ heapq.heappop(nums) # Pop an element from the heap
 # and the returned value from that function is then used to rank that element in the heap
 heapq.nlargest(k, iterable, key = fun)
 heapq.nsmallest(k, iterable, key = fun)
+
+#------- SET-------#
+
+set = {1,2,3}
+
+set.add(item)
+set.remove(item)
+set.discard(item) | set.remove(item) # removes item | remove will throw error if item is not there, discard will not
+set.pop() # removes random item (since unordered)
+
+set.isdisjoint(anotherSet) # returns true if no common elements
+set.issubset(anotherSet) # returns true if all elements from anotherSet is present in original set
+set.issuperset(anotherSet) # returns true if all elements from original set is present in anotherSet
+
+set.difference(anotherSet) # returns set containing items ONLY in first set
+set.difference_update(anotherSet) # removes common elements from first set [no new set is created or returned]
+set.intersection(anotherSet) # returns new set with common elements
+set.intersection_update(anotherSet) # modifies first set keeping only common elements
+set.symmetric_difference(anotherSet) # returns set containing all non-common elements of both sets
+set.symmetric_difference_update(anotherSet) # same as symmetric_difference but changes are made on original set
+
+set.union(anotherSet) # ...
+set.update(anotherSet) # adds anotherSet without duplicate
