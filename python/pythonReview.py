72855e63a6ea9fa396e32e517c429d305fbca41a
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-18 17:10:38 -0700

    TUPLE,STRING

--- python/pythonReview.py
+++ python/pythonReview.py
@@ -127,7 +127,7 @@ queue.count() # obvious
 queue.reverse() # reverses order of queue elements
 
 
-#------- HEAPQ-------#
+#------- HEAPQ -------#
 
 import heapq # (minHeap by Default)
 
@@ -146,7 +146,7 @@ heapq.heappop(nums) # Pop an element from the heap
 heapq.nlargest(k, iterable, key = fun)
 heapq.nsmallest(k, iterable, key = fun)
 
-#------- SET-------#
+#------- SET -------#
 
 set = {1,2,3}
 
@@ -168,3 +168,59 @@ set.symmetric_difference_update(anotherSet) # same as symmetric_difference but c
 
 set.union(anotherSet) # ...
 set.update(anotherSet) # adds anotherSet without duplicate
+
+
+#------- TUPLE -------#
+
+tuple = (1,2,3,1)
+
+tuple.count(1) # returns occurence of an item
+tuple.index(1) # returns index of 1 in array
+
+
+#------- STRING -------#
+
+# ** split Function **
+#The split() method breaks up a string at the specified separator and returns a list of strings.
+text = 'Python is a fun programming language'
+
+# split the text from space
+print(text.split(' '))
+
+#convert string to list
+s="abcd"
+s=list(s)
+
+# Output: ['Python', 'is', 'a', 'fun', 'programming', 'language']
+
+# ** count Function **
+#The count() method returns the number of occurrences of a substring in the given string.
+#Example
+message = 'python is popular programming language'
+# number of occurrence of 'p'
+print('Number of occurrence of p:', message.count('p')) # Output: Number of occurrence of p: 4
+
+#The isnumeric() method returns True if all characters in a string are numeric characters. If not, it returns False.
+s = '1242323'
+print(s.isnumeric()) #Output: True
+
+#The find() method returns the index of first occurrence of the substring (if found). If not found, it returns -1.
+# check the index of 'fun'
+print(message.find('fun')) # Output: 12
+
+#The isalnum() method returns True if all characters in the string are alphanumeric (either alphabets or numbers). If not, it returns False.
+
+name = "M3onica Gell22er "
+print(name.isalnum()) # Output : False
+
+#The isalpha() method returns True if all characters in the string are alphabets. If not, it returns False
+name = "Monica"
+print(name.isalpha()) #output true
+
+#other imp functions
+string.strip([chars]) #The strip() method returns a copy of the string by removing both the leading and the trailing characters (based on the string argument passed).
+string.upper() #he upper() method converts all lowercase characters in a string into uppercase characters and returns it.
+string.lower() #The lower() method converts all uppercase characters in a string into lowercase characters and returns it.
+string.islower()
+string.isdigit()
+string.isupper()
