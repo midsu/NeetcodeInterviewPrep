d3cb28dc2ee80dd4e7b5de02b03921b9a4610bdb
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-20 13:34:17 -0700

    added  example and explainstion

--- validParantheses.py
+++ validParantheses.py
@@ -1,5 +1,20 @@
 '''
+Given a string s containing just the characters '(', ')', '{', '}', '[',  and ']'
+determine if the input string is valid
 
+An input string is valid if:
+    1- Open brackets must be closed by the same type of brackets
+    2- Open brackets must be close in the correct order
+
+example:
+Input: s = "()"
+Output: true
+
+Input: s = "()[]{}"
+Output: true
+
+Input: s = "([)]"
+Output: false
 '''
 class Solution:
     def isValid(self, s: str) -> bool:
