9181af18c2a12bd2638714d75752797d0592edd2
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-20 13:29:57 -0700

    commented

--- validParantheses.py
+++ validParantheses.py
@@ -1,20 +1,19 @@
 '''
 
 '''
-
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        closeToOpen = {")": "(", "]": "[", "}": "{"}
+        stack = []  # Initialize an empty stack to store opening brackets
+        # Dictionary to store the mapping of closing brackets to opening brackets
+        closeToOpen = {")": "(", "]": "[", "}": "{"} 
 
-        for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
-                    stack.pop()
+        for c in s:  # Iterate through each character in the input string 's'
+            if c in closeToOpen:  # Check if the character is a closing bracket
+                if stack and stack[-1] == closeToOpen[c]:  # If the stack is not empty and the top element matches the corresponding opening bracket
+                    stack.pop()  # Pop the top element from the stack (matching opening bracket found)
                 else:
-                    return False
+                    return False  # If the stack is empty or the top element doesn't match the corresponding opening bracket, the string is invalid
             else:
-                stack.append(c)
+                stack.append(c)  # If the character is an opening bracket, add it to the stack
 
-        return True if not stack else False
+        return True if not stack else False  # If the stack is empty after processing all characters, the string is valid; otherwise, it is invalid
