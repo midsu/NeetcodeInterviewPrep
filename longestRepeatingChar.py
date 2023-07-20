09268e9fda1eaf94b3b82da6a657f2fb2dc348ea
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-20 12:18:08 -0700

    optimized

--- longestRepeatingChar.py
+++ longestRepeatingChar.py
@@ -7,6 +7,28 @@ Return the length of the longest substring containting
 the same letter you can get after oerforming the above operation.
 '''
 
+# OPTIMIZED
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}  # Dictionary to store the frequency count of characters in the current window
+        res = 0     # Variable to store the maximum length of the substring with the same characters
+
+        maxf = 0
+        l = 0       # Left pointer of the sliding window
+        for r in range(len(s)):  # Right pointer of the sliding window (expands the window to the right)
+            # Update the count of the character at the right pointer and handle its initial count as 0
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+
+            # Check if the number of characters to be replaced (total characters in window - max character count) exceeds 'k'
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1  # Reduce the count of the character at the left pointer
+                l += 1            # Move the left pointer to the right to shrink the window
+            # Update the result with the maximum length of the substring with the same characters
+            res = max(res, r - l + 1) # r - l + 1 is the size of the window/substring
+
+        return res
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}  # Dictionary to store the frequency count of characters in the current window
