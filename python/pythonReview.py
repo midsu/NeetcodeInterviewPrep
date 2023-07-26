6ad33c17b45cb3fe9cfe9d26a66cdca31f06fba8
Hamid Suha <hsuha@csu.fullerton.edu>
2023-07-25 21:48:16 -0700

    Formating

--- python/pythonReview.py
+++ python/pythonReview.py
@@ -290,3 +290,61 @@ left() right() logic is also same as above.
 # returns ascii value of the character , Example ord("a") = 97
 ** chr(int) ** 
 #return character of given ascii value , Example chr(97) = "a"
+
+
+#------- Formating -------#
+
+# Python program showing how to
+# multiple input using split
+ 
+# taking two inputs at a time
+x, y = input("Enter a two value: ").split()
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+print()
+ 
+# taking three inputs at a time
+x, y, z = input("Enter a three value: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
+print()
+ 
+# taking two inputs at a time
+a, b = input("Enter a two value: ").split()
+print("First number is {} and second number is {}".format(a, b))
+print()
+ 
+# taking multiple inputs at a time
+# and type casting using list() function
+x = list(map(int, input("Enter a multiple value: ").split()))
+print("List of students: ", x)
+# Python program showing
+# how to take multiple input
+# using List comprehension
+ 
+# taking two input at a time
+x, y = [int(x) for x in input("Enter two value: ").split()]
+print("First Number is: ", x)
+print("Second Number is: ", y)
+print()
+ 
+# taking three input at a time
+x, y, z = [int(x) for x in input("Enter three value: ").split()]
+print("First Number is: ", x)
+print("Second Number is: ", y)
+print("Third Number is: ", z)
+print()
+ 
+# taking two inputs at a time
+x, y = [int(x) for x in input("Enter two value: ").split()]
+print("First number is {} and second number is {}".format(x, y))
+print()
+ 
+# taking multiple inputs at a time
+x = [int(x) for x in input("Enter multiple value: ").split()]
+print("Number of list is: ", x)
+
+# taking multiple inputs at a time separated by comma
+x = [int(x) for x in input("Enter multiple value: ").split(",")]
+print("Number of list is: ", x)
