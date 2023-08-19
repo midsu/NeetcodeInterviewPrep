f3de87372847881e7c7e313e6631d38fbbd2d165
Hamid Suha <hsuha@csu.fullerton.edu>
2023-08-19 16:56:50 -0700

    group anagram using hashmap

--- groupAnagram.py
+++ groupAnagram.py
@@ -10,8 +10,9 @@
 
 
 # Solution using Hashmap
+'''
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[list[str]]:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         res = defaultdict(list) # mapping charCount to List of Anagram
 
         for s in strs:
@@ -28,5 +29,48 @@ class Solution:
             res[tuple(count)].append(s)
 
         return res.values()
-
+'''
 # O(m * n ) time complexity
+
+
+''' Another Solution '''
+# Solution using Hashmap
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        # Create a defaultdict to store anagrams
+        anagram_map = defaultdict(list)
+        result = []
+
+        # Loop through the list of strings
+        for s in strs:
+            # Convert the string to a sorted tuple of characters
+            sorted_s = tuple(sorted(s))
+            # Append the original string to the corresponding anagram group
+            anagram_map[sorted_s].append(s)
+
+        # Loop through the groups of anagrams
+        for value in anagram_map.values():
+            # Append each group to the result list
+            result.append(value)
+
+        return result
+
+# Test Cases
+test_case_1 = ["eat", "tea", "tan", "ate", "nat", "bat"]
+test_case_2 = ["hello", "world", "goodbye"]
+test_case_3 = ["listen", "silent", "elbow", "below", "state", "taste"]
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Call the groupAnagrams function on each test case
+output_1 = solution.groupAnagrams(test_case_1)
+output_2 = solution.groupAnagrams(test_case_2)
+output_3 = solution.groupAnagrams(test_case_3)
+
+# Print the results
+print("Test case 1: ",output_1)
+print("Test case 2: ",output_2)
+print("Test case 3: ",output_3)
+
