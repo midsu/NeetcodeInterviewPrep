bb1f72106fd9705eeac7717cfed8987c8a803c74
Hamid Suha <hsuha@csu.fullerton.edu>
2023-09-02 14:21:23 -0700

    New qustion

---
+++ linkedListCycle.py
@@ -0,0 +1,14 @@
+"""
+Linked List Cycle
+
+Input: head = [3, 2, 0 , -4], position = 1  // a linked list
+Output: True
+Explaination: There is a cycle in the linked list, 
+              where the tail connects to the 1st node (0-indexed).
+
+Input: head = [1, 2], position = 0  // a linked list
+Output: True
+Explaination: There is a cycle in the linked list,
+              where tail connects to the 0th node.
+"""
+
